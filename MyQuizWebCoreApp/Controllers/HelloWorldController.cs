d3e60149157810539e51669d214939444cb0b57f
Katherine Hambley <khambley@franklinenergy.com>
2020-09-09 14:22:11 -0500

    Changed NumTimes parameter to ID in HelloWorldController

--- MyQuizWebCoreApp/Controllers/HelloWorldController.cs
+++ MyQuizWebCoreApp/Controllers/HelloWorldController.cs
@@ -17,9 +17,10 @@ namespace MyQuizWebCoreApp.Controllers
 
         // GET: /HelloWorld/Welcome
         // https://localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4
-        public string Welcome(string name, int numTimes = 1)
+        // /HelloWorld/Welcome/3?name=Rick
+        public string Welcome(string name, int ID = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
         }
     }
 }
