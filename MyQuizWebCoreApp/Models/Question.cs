5abc8e2994d0fa48a3e60e59002b6847fd419164
Katherine Hambley <khambley@franklinenergy.com>
2020-09-09 15:14:37 -0500

    Added QuestionsController with scaffolded views

--- MyQuizWebCoreApp/Models/Question.cs
+++ MyQuizWebCoreApp/Models/Question.cs
@@ -7,5 +7,13 @@ namespace MyQuizWebCoreApp.Models
 {
     public class Question
     {
+        public int QuestionId { get; set; }
+        public string QuestionText { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string WrongAnswer1 { get; set; }
+        public string? WrongAnswer2 { get; set; }
+        public string? WrongAnswer3 { get; set; }
+        public string? WrongAnswer4 { get; set; }
+
     }
 }
--- MyQuizWebCoreApp/Startup.cs
+++ MyQuizWebCoreApp/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyQuizWebCoreApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyQuizWebCoreApp
 {
@@ -24,6 +26,9 @@ namespace MyQuizWebCoreApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<QuizDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("QuizDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
