fa3ae1112acfc5207f54702fe097b7cac488ba8b
Katherine Hambley <khambley@franklinenergy.com>
2020-09-09 14:15:56 -0500

    Added initial project files

---
+++ MyQuizWebCoreApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyQuizWebCoreApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // Get: /HelloWorld
+        public string Index()
+        {
+            return "This is my Index (default) action";
+        }
+
+        // GET: /HelloWorld/Welcome
+        // https://localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
