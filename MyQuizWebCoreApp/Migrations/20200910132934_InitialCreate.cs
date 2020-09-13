77066c310fa20186f5acba92491eca2242e084a6
Katherine Hambley <khambley@franklinenergy.com>
2020-09-13 09:59:25 -0500

    Added Db Initial Migration

---
+++ MyQuizWebCoreApp/Migrations/20200910132934_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyQuizWebCoreApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    QuestionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionText = table.Column<string>(nullable: true),
+                    CorrectAnswer = table.Column<string>(nullable: true),
+                    WrongAnswer1 = table.Column<string>(nullable: true),
+                    WrongAnswer2 = table.Column<string>(nullable: true),
+                    WrongAnswer3 = table.Column<string>(nullable: true),
+                    WrongAnswer4 = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.QuestionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
